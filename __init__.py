538461b22f7bea5398d4c479d9133239756241da
Brad Neuroth <neurothbr@epb.net>
2023-06-11 16:09:18 -0400

    init commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# epb_energy/__init__.py
+
+"""The EPB Energy integration."""
+
+DOMAIN = "epb_energy"
+
+
+async def async_setup(hass, config):
+    """Set up the EPB Energy component."""
+    return True
---
+++ const.py
@@ -0,0 +1,5 @@
+# epb_energy/const.py
+
+"""Constants for the EPB Energy integration."""
+
+DOMAIN = "epb_energy"
