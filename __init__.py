b91c931a2baed621886312732752081fdedee6e5
Brad Neuroth <neurothbr@epb.net>
2023-06-11 16:29:33 -0400

    more work

--- __init__.py
+++ __init__.py
@@ -1,10 +1,20 @@
 # epb_energy/__init__.py
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+from const import DOMAIN, CONF_USERNAME, CONF_PASSWORD
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import Config, HomeAssistant
 
 """The EPB Energy integration."""
 
-DOMAIN = "epb_energy"
+DOMAIN = DOMAIN
 
 
-async def async_setup(hass, config):
-    """Set up the EPB Energy component."""
+async def async_setup(hass: HomeAssistant, config: Config):
+    """Set up this integration using YAML is not supported."""
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up this integration using UI."""
     return True
--- const.py
+++ const.py
@@ -3,3 +3,8 @@
 """Constants for the EPB Energy integration."""
 
 DOMAIN = "epb_energy"
+
+
+CONF_ENABLED = "enabled"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
