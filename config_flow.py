6fceaee55857d1eb3c0943797131f5f12b9fc6ef
Brad Neuroth <neurothbr@epb.net>
2023-06-12 09:09:42 -0400

    login validation

--- config_flow.py
+++ config_flow.py
@@ -3,16 +3,64 @@
 """Config flow for EPB Energy integration."""
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
+from homeassistant import config_entries, exceptions
+from homeassistant.util import aiohttp
 
 from .const import DOMAIN
+
+
+async def validate_input(hass: HomeAssistant, data: dict) -> dict[str, Any]:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from DATA_SCHEMA with values provided by the user.
+    """
+    # Validate the data can be used to set up a connection.
+    login_url = "https://api.epb.com/web/api/v1/login/"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                login_url,
+                json={"username": data["username"], "password": data["password"], "grant_type": "PASSWORD"},
+        ) as response:
+            if not (response.status >= 200 and response.status < 300):
+                raise InvalidAuth
+
+    # If your PyPI package is not built with async, pass your methods
+    # to the executor:
+    # await hass.async_add_executor_job(
+    #     your_validate_func, data["username"], data["password"]
+    # )
+
+    # If you cannot connect:
+    # throw CannotConnect
+    # If the authentication is wrong:
+    # InvalidAuth
+
+    # Return info that you want to store in the config entry.
+    # "Title" is what is displayed to the user for this hub device
+    # It is stored internally in HA as part of the device config.
+    # See `async_step_user` below for how this is used
+    return {"title": data["host"]}
+
 
 class EPBEnergyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle an EPB Energy config flow."""
 
+    errors = {}
+
     async def async_step_user(self, user_input=None):
         """Handle a flow initiated by the user."""
+
+        errors = {}
         if user_input is None:
             return await self._show_config_form()
+        else:
+            try:
+                info = await validate_input(self.hass, user_input)
+
+                return self.async_create_entry(title=info["title"], data=user_input)
+            except InvalidAuth:
+                errors["base"] = "invalid_auth"
 
         # Validate and store user input
 
@@ -22,6 +70,7 @@ class EPBEnergyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 "username": user_input["username"],
                 "password": user_input["password"],
             },
+            errors=errors
         )
 
     async def _show_config_form(self, errors=None):
